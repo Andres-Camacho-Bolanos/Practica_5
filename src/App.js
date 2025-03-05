77d400b07d6f253a4e912d1e728b7176bad704ef
Andres-Camacho-Bolanos <andrescamacho058@comunidad.unam.mx>
2025-03-04 21:30:47 -0600

    Práctica 5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Escuela Particular
+          </p>
+
+
+        <div className={"Access-button"}>
+          <Button variant="contained" color="primary">
+            Acceso
+          </Button>
+        </div>
+
+        </header>
+
+        </div>
   );
 }
 
